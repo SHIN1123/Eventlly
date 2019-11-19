6dc919e113b739203003506493c15b4923de4305
SHIN1123 <shinnichi081123@icloud.com>
2019-11-19 10:28:04 +0900

    dm

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
   has_many :comments
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:index, :create, :show]
   end
+resources :messages, only: [:create]
+resources :rooms, only: [:create, :show, :index]
 end
